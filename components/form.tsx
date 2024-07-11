c885e18c70960a52d9024d32e67307bc3fbfe7de
devHksingh <hksingh.dev@gmail.com>
2024-07-11 15:40:02 +0530

    add functionality for sign in in dashboard

--- components/form.tsx
+++ components/form.tsx
@@ -13,6 +13,7 @@ import { toast } from 'sonner'
 import { createCompletion } from '@/app/actions'
 import { useFormStatus } from 'react-dom'
 import { cn } from '@/lib/utils'
+import { SignedIn, SignedOut, SignInButton } from '@clerk/nextjs'
 
 export default function Form() {
   async function action(formData:FormData) {
@@ -55,19 +56,30 @@ export default function Form() {
 
 function SubmitButton(){
   const {pending} = useFormStatus()
-
   return (
     <>
-      <Button
-        size={'sm'}
-        type='submit'
-        className={cn('mt-3 w-full rounded-lg', pending && 'animate-pulse')}
-        
-      >
-        {
-          pending ? 'Working on it ....':'Submit'
-        }
-      </Button>
+      <SignedIn>
+        <Button
+          size='sm'
+          type='submit'
+          className={cn('mt-3 w-full rounded-lg', pending && 'animate-pulse')}
+        >
+          {pending ? 'Working on it...' : 'Submit'}
+        </Button>
+      </SignedIn>
+
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <Button
+            size='sm'
+            type='button'
+            variant='secondary'
+            className='mt-3 w-full rounded-lg'
+          >
+            Sign in to start
+          </Button>
+        </SignInButton>
+      </SignedOut>
     </>
   )
 }
